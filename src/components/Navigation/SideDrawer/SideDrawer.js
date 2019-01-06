f395796b8675c4508546c7e4b0108e2c640940e2
Vinayreddyyp <ypvinayreddy@gmail.com>
2019-01-06 15:52:02 -0700

    add sideDrawer to the app

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 import classes from './SideDrawer.css';
 
 const SideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if(props.Open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
     return (
-      <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-         <Logo />
-      </div>
-           <NavigationItems />
-       </div>
+     <Aux>
+     <Backdrop show={props.open} clicked={props.closed} />
+          <div className={attachedClasses.join(' ')}>
+          <div className={classes.Logo}>
+             <Logo />
+          </div>
+               <NavigationItems />
+           </div>
+      </Aux>
     )
 }
 export default SideDrawer;
